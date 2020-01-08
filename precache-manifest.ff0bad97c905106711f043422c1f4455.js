7a66ea1b930cf1a8ddcf2bb23c181d7da0ee189c
avyjyo11 <avy.jyo11@gmail.com>
2020-01-08 20:32:14 +0545

    Updates

---
+++ precache-manifest.ff0bad97c905106711f043422c1f4455.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd7d9495c19a46e99547c25f04da81e4",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "7396cf4183941bc9213f",
+    "url": "/to-do-list/static/css/main.28c52548.chunk.css"
+  },
+  {
+    "revision": "151456b9ee3ae756709f",
+    "url": "/to-do-list/static/js/2.d9165343.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/to-do-list/static/js/2.d9165343.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7396cf4183941bc9213f",
+    "url": "/to-do-list/static/js/main.d8be4714.chunk.js"
+  },
+  {
+    "revision": "eb4acb13890cbe06988a",
+    "url": "/to-do-list/static/js/runtime-main.2a06a75b.js"
+  }
+]);
